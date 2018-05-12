19b7a7c5bb324b971622904ba025ba5a7529cea5
JamisonOrdway <jamisonordway@gmail.com>
2018-05-12 16:13:50 -0600

    Database setup WIP

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214181533) do
+ActiveRecord::Schema.define(version: 20180512215736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "title"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
