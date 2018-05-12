f488d0c959e454bbf39e91db1a2ec1f21b50da03
JamisonOrdway <jamisonordway@gmail.com>
2018-05-12 17:46:14 -0600

    TESTS category model

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe Category do
+  describe 'validations' do
+    context 'invalid attributes' do
+      it 'is invalid without a title' do
+        category = Category.new()
+        expect(category).to be_invalid
+      end
+      it 'has a unique title' do
+        Category.create(name: 'Finances')
+        category = Category.new(name: 'Finances')
+        expect(category).to be_invalid
+      end
+    end
+
+    context 'valid attributes' do
+      it 'is valid with a unique title' do
+        category = Category.new("Engineering")
+        expect(category).to be_valid
+      end
+    end
+  end
+end
