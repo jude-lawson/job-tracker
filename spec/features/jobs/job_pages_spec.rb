aeba54b0586146f7cab6a2b2d4f8582905d337f7
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-15 11:52:32 -0600

    TESTS seeing all jobs and seeing a specific job for a company

--- spec/features/jobs/job_pages_spec.rb
+++ spec/features/jobs/job_pages_spec.rb
@@ -1,25 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe 'Job Pages' do
+
+  before :each do
+    @company1 = Company.create!(name: "ESPN")
+    @category = Category.create(title: 'Web Development')
+    @job1 = @company1.jobs.create!(title: "Developer", description: 'This is a cool job', level_of_interest: 70, city: "Denver", category_id: @category.id)
+    @job2 = @company1.jobs.create!(title: "Developer Too", description: 'This is a not cool job', level_of_interest: 70, city: "Denver", category_id: @category.id)
+  end
+
   context 'User visits /jobs/new' do
     it 'they can create a new job' do
-      company = Company.create!(name: 'ESPN')
-      category = Category.create!(title: 'Web Development')
-      visit new_company_job_path(company)
+      visit new_company_job_path(@company1)
 
       fill_in 'job[title]', with: 'Developer'
       fill_in 'job[description]', with: 'So fun!'
       fill_in 'job[level_of_interest]', with: 80
       fill_in 'job[city]', with: 'Denver'
-      # select 'Web Development', froml: 'Category'
+      select 'Web Development', from: 'job[category_id]'
+
 
       click_button 'Create Job'
 
-      expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
+      expect(current_path).to eq("/companies/#{@company1.id}/jobs/#{Job.last.id}")
       expect(page).to have_content('ESPN')
       expect(page).to have_content('Developer')
       expect(page).to have_content('80')
       expect(page).to have_content('Denver')
     end
   end
+
+  context 'User visits /company/:company_id/jobs' do
+    it 'they should see all jobs for a company' do
+      visit company_jobs_path(@company1)
+
+      expect(page).to have_content(@company1.name)
+      expect(page).to have_content(@job1.title)
+      expect(page).to have_content(@job2.title)
+    end
+  end
+
+  context 'User visits company/:id/jobs/:id' do
+    it 'the user should see a specific jobs for a specific company' do
+      
+      visit company_job_path(@company1, @job1)
+  
+      expect(page).to have_content(@company1.name)
+      expect(page).to have_content(@job1.title)
+      expect(page).to have_content(@job1.description)
+      expect(page).to have_content(@job1.level_of_interest)
+
+      expect(page).to_not have_content(@job2.title)
+      expect(page).to_not have_content(@job2.description)
+    end
+
+  context 'User visits /company/:id/jobs/:id/edit' do
+    it 'they should be able to update a job' do
+      visit edit_company_job_path(@company, @job1)
+
+      
+    end
+  end
+
+    it 'they should be able to delete a job' do
+    end
+  end
+
 end
