faec3d40c8c5c1ab1cdd33a5fa07fa98fcc3fc92
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-15 22:00:43 -0600

    Implements some jobs feature tests (new, show, and edit). Category is WIP

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,22 +1,20 @@
 class JobsController < ApplicationController
   def index
-    @company = Company.find(params[:company_id])
-    @jobs = @company.jobs
+    @jobs = Job.all
   end
 
   def new
-    @company = Company.find(params[:company_id])
     @job = Job.new()
     @categories = Category.all
     @category_options = @categories.map(&:title).zip(@categories.map(&:id)) 
   end
 
   def create
-    @company = Company.find(params[:company_id])
+    @company = Company.find(job_params[:company_id])
     @job = @company.jobs.create!(job_params)
     if @job.save
       flash[:success] = "You created #{@job.title} at #{@company.name}"
-      redirect_to company_job_path(@company, @job)
+      redirect_to job_path(@job)
     else
       render :new
     end
@@ -28,26 +26,25 @@ class JobsController < ApplicationController
 
   def edit
     @job = Job.find(params[:id])
-    @company = Company.find(params[:company_id])
   end
 
-  def update
-    @job = Job.find(params[:id])
-    @company = Company.find(params[:company_id])
-    @company.jobs.update(job_params)
-    redirect_to company_job_path(@company, @job)
-  end
+  # def update
+  #   @job = Job.find(params[:id])
+  #   @company = Company.find(params[:company_id])
+  #   @company.jobs.update(job_params)
+  #   redirect_to company_job_path(@company, @job)
+  # end
 
-  def destroy
-    @job = Job.find(params[:id])
-    @company = Company.find(params[:company_id])
-    Job.destroy(params[:id])
-    redirect_to company_jobs_path(@company)
-  end
+  # def destroy
+  #   @job = Job.find(params[:id])
+  #   @company = Company.find(params[:company_id])
+  #   Job.destroy(params[:id])
+  #   redirect_to company_jobs_path(@company)
+  # end
 
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :level_of_interest, :city, :category_id)
+    params.require(:job).permit(:title, :description, :level_of_interest, :city, :category_id, :company_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :companies do
-    resources :jobs
-  end
+  # resources :companies do
+  #   resources :jobs, except: [:new, :create]
+  # end
+
+  # resources :jobs, only: [:new, :create]
+  resources :jobs
+  resources :companies, only: [:show]
+  resources :categories, only: [:new, :create]
 end
