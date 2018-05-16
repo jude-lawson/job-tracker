f4b12bb12d714bf3a951653f0a964a996ed72146
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-15 20:48:42 -0600

    Fixing routes for jobs WIP

--- spec/features/jobs/job_pages_spec.rb
+++ spec/features/jobs/job_pages_spec.rb
@@ -9,24 +9,26 @@ RSpec.describe 'Job Pages' do
     @job2 = @company1.jobs.create!(title: "Developer Too", description: 'This is a not cool job', level_of_interest: 70, city: "Denver", category_id: @category.id)
   end
 
-  context 'User visits /jobs/new' do
-    it 'they can create a new job' do
-      visit new_company_job_path(@company1)
+  context '/jobs/new' do
+    describe 'A user visits the new job page' do
+      it 'they can create a new job' do
+        visit new_company_job_path(@company1)
 
-      fill_in 'job[title]', with: 'Developer'
-      fill_in 'job[description]', with: 'So fun!'
-      fill_in 'job[level_of_interest]', with: 80
-      fill_in 'job[city]', with: 'Denver'
-      select 'Web Development', from: 'job[category_id]'
+        fill_in 'job[title]', with: 'Developer'
+        fill_in 'job[description]', with: 'So fun!'
+        fill_in 'job[level_of_interest]', with: 80
+        fill_in 'job[city]', with: 'Denver'
+        select 'Web Development', from: 'job[category_id]'
 
 
-      click_button 'Create Job'
+        click_button 'Create Job'
 
-      expect(current_path).to eq("/companies/#{@company1.id}/jobs/#{Job.last.id}")
-      expect(page).to have_content('ESPN')
-      expect(page).to have_content('Developer')
-      expect(page).to have_content('80')
-      expect(page).to have_content('Denver')
+        expect(current_path).to eq("/companies/#{@company1.id}/jobs/#{Job.last.id}")
+        expect(page).to have_content('ESPN')
+        expect(page).to have_content('Developer')
+        expect(page).to have_content('80')
+        expect(page).to have_content('Denver')
+      end
     end
   end
 
@@ -83,7 +85,6 @@ RSpec.describe 'Job Pages' do
         click_link('Delete')
       end
 
-      save_and_open_page
       expect(current_path).to eq(company_jobs_path(@company1))
       expect(page).to have_content(@company1.name)
       expect(page).to have_content(@job2.title)
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Job do
+RSpec.describe Job do
   describe "validations" do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:level_of_interest) }
@@ -9,13 +9,8 @@ describe Job do
   end
 
   describe "relationships" do
-    it "belongs to a company" do
-      job = Job.new(title: "Software", level_of_interest: 70, description: "Wahooo")
-      expect(job).to respond_to(:company)
-    end
-    it 'belongs to a category' do
-      job = Job.new(title: "UX", level_of_interest: 50, description: "Thing")
-      expect(job).to respond_to(:category)
-    end
+    it { should have_many(:comments) }
+    it { should belong_to(:company) }
+    it { should belong_to(:category) }
   end
 end
