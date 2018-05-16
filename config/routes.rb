777e505433bb0653c7614e29039cde5e42c19331
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-16 12:19:57 -0600

    Implements 2 out of 6 points of analysis functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,14 @@
 Rails.application.routes.draw do
-  # resources :companies do
-  #   resources :jobs, except: [:new, :create]
-  # end
-
-  # resources :jobs, only: [:new, :create]
-
-  # resources :jobs
-#   resources :companies, only: [:show]
-
   resources :jobs do
     resources :comments
   end
   resources :companies do
     resources :contacts
     resources :jobs, only: [:index]
-    #resources :jobs
   end
   resources :categories, only: [:index, :new, :create, :destroy, :edit, :update] do
     resources :jobs, only: [:index]
   end
+
+  get '/dashboard', to: 'jobs#dashboard'
 end
