fc16a01681fa28166d0dc256922e055e239bc269
JamisonOrdway <jamisonordway@gmail.com>
2018-05-16 10:08:10 -0600

    Fixes comment model tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,23 +5,23 @@ RSpec.describe Comment do
     company = Company.create!(name: 'The Company')
     category = Category.create!(title: 'Cool Jobssss')
     city = City.create!(name: 'Denver')
-    @job = Job.create!(title: 'Cool Job', level_of_interest: 2, description: 'The coolest job', company_id: company.id, category_id: category.id, city_id: city.id)
+    @job = Job.create!(title: 'Cool Job', level_of_interest: 2, description: 'The coolest job', company_id: company.id, category_id: category.id, city: city.name)
   end
 
   describe 'validations' do
-    xit 'should be invalid without a title' do
+    it 'should be invalid without a title' do
       comment = @job.comments.new(body: "Worst comment ever")
 
       expect(comment).to be_invalid
     end
 
-    xit 'should be invalid without a body' do
+  it 'should be invalid without a body' do
       comment = @job.comments.new(title: "Best comment ever")
 
       expect(comment).to be_invalid
     end
 
-    xit 'should be valid with a title and a body' do
+    it 'should be valid with a title and a body' do
       comment = @job.comments.new(title: "Good", body: "I hope they hire me")
 
       expect(comment).to be_valid
